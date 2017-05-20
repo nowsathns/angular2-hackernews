d63eb04f88a771b5f037dd58fcc9544dafadf9cc
Nowshath H <nowshath.h@ivygdcad.ivy-support.com>
2017-05-20 17:42:55 +0530

    Updated Code

---
+++ src/app/item-details/item-details.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ItemDetailsComponent } from './item-details/item-details.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ItemDetailsComponent]
+})
+export class ItemDetailsModule { }
